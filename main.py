a9bb21a7d043647b74eb41784ed0eafec8bdb051
Marc Escolà <79698889+marcescola98@users.noreply.github.com>
2025-11-19 20:00:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ import hf_kernels
 def main():
   input_name=str.input('write the name of your file')
   output_name= input_file + '.out'
+  
   pass
 
+def read_input(input_name):
+  with open(input_name, r) as file:
+    s=file.read()
+    return print(s)
+    
